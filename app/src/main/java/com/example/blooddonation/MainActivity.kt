a63a19d64c065dad61c9f1004706dc6f6c5a0d5d
Lookman98 <mikeyluqman98@gmail.com>
2020-11-12 05:11:06 +0800

    lol

---
+++ app/src/main/java/com/example/blooddonation/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.blooddonation
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.blooddonation.ui.DonateFragment
+import com.example.blooddonation.ui.HomeFragment
+import com.example.blooddonation.ui.SettingsFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment();
+        val donateFragment = DonateFragment();
+        val settingsFragment = SettingsFragment();
+
+        makeCurrentFragment(homeFragment);
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.navigation_home -> makeCurrentFragment(homeFragment)
+                R.id.navigation_donate -> makeCurrentFragment(donateFragment)
+                R.id.navigation_settings -> makeCurrentFragment(settingsFragment)
+            }
+            true
+        }
+
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper,fragment)
+            commit();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/blooddonation/ui/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.example.blooddonation.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+import com.example.blooddonation.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val homeView = inflater.inflate(R.layout.fragment_home, container, false)
+        return homeView
+    }
+}
